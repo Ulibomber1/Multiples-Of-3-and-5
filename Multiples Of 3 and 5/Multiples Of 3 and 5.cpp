6e71fc3827ed1038e7dd42ae834ef8d877a2dac4
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2022-02-28 22:19:15 -0800

    Optimized code to use less memory by removing use of arrays. Changed the comments to reflect the change in approach. Why did I ever use arrays for something this simple?

--- Multiples Of 3 and 5/Multiples Of 3 and 5.cpp
+++ Multiples Of 3 and 5/Multiples Of 3 and 5.cpp
@@ -7,34 +7,17 @@ using namespace std;
 int main()
 {
     int sum = 0;
-    //Find the multiples of 3 or 5, then putting them into a fixed array. 
-    //Any number that is not a multiple of 3 or 5 is defaulted to zero.
 
-    int multiples[1000];
+    //Find the multiples of 3 or 5, then add them to sum
     for (int i = 0; i < 1000; i++)
     {
         if (i % 3 == 0 or i % 5 == 0)
-            multiples[i] = i;
-        else
-            multiples[i] = 0;
-        cout << multiples[i] << " ";
+        {
+            sum += i;
+            //cout << i << " "; // debug output, uncomment to check algorithm
+        }
     }
     cout << endl;
 
-    //Sum the array to get the answer
-    for (int i = 0; i < 1000; i++)
-        sum += multiples[i];
-
     cout << "The sum of all multiples of 3 and 5 under 1000 is " << sum;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
